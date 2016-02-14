94d7989d22f9682af83b931b04df5f0f20f42381
hosunglee <lee.ho.sung@gmail.com>
2016-02-15 00:25:03 +0900

    First unit test and url mapping, dummy view

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from lists.views import home_page
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', home_page, name='home')
+    # url(r'^admin/', admin.site.urls),
 ]
