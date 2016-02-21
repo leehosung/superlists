74ab52aad35679093c74cf8bfd2c5b0ff05b4902
hosunglee <lee.ho.sung@gmail.com>
2016-02-22 00:48:25 +0900

    refactor functional test

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retreive_it_later(self):
         # Edith 는 새로운 멋진 할일관리 싸이트에 대해서 들었습니다.
         # 홈페이지를 확인해 봅니다.
@@ -35,10 +40,7 @@ class NewVisitorTest(unittest.TestCase):
         # 그녀가 엔터를 치면 페이지가 업데이트 되고 리스트에서
         # "1: Buy peacock feathers" 를 볼 수 있습니다.
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 여전히 텍스트 박스가 있습니다.
         # "Use peacock feathers to make a fly"를 입력합니다.
@@ -47,10 +49,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 다시 한번 페이지가 업데이트 됩니다. 그리고 두 항목 모두를 볼 수 있습니다.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith 는 싸이트가 그녀의 리스트를 기억하고 있는 지 궁금합니다.
         # 그녀는 싸이트가 유일한 URL을 만들어 둔것을 볼 수 있습니다.
