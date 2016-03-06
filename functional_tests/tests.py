b17e57f5d29c36a161c019c4b63c01e7435259a5
hosunglee <lee.ho.sung@gmail.com>
2016-03-06 22:56:48 +0900

    move sqlite database outside of main source tree

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,25 @@
+import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                print(cls.server_url)
+                return
+            super().setUpClass()
+            cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Chrome('drivers/chrome')
@@ -20,7 +36,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retreive_it_later(self):
         # Edith 는 새로운 멋진 할일관리 싸이트에 대해서 들었습니다.
         # 홈페이지를 확인해 봅니다.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # 페이지의 제목이 할일관리인 것을 확인 합니다.
         self.assertIn('To-Do', self.browser.title)
@@ -61,7 +77,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Chrome('drivers/chrome')
 
         # Francis 는 홈페이지를 방문합니다. 홈페이지에는 Edith 의 흔적이 없어야 합니다.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -87,7 +103,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
 
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # 입력박스는 가운데 정렬이 되어 있습니다.
